1ea7ea5370a35a764115b13301464ce5c5659b60
Mohamad Elgaml <melgaml@icloud.com>
2023-10-26 02:53:34 +0300

    notification

---
+++ index.js
@@ -0,0 +1,7 @@
+function readunread() {
+  document.getElementById("notbutt").value = "0";
+  document.getElementById("notbutt").style.backgroundColor = "white";
+  document.getElementById("selected1").style.backgroundColor = "white";
+  document.getElementById("selected2").style.backgroundColor = "white";
+  document.getElementById("selected3").style.backgroundColor = "white";
+}
